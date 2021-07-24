4830a08615b6d1a4d2217d87283beab96fed775b
Serranos Miser <serrano.miser@gmail.com>
2021-07-24 19:47:31 +0100

    Fix JAVA_HOME path

--- bash/.bashrc.java
+++ bash/.bashrc.java
@@ -1,7 +1,13 @@
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home"
 export PATH=$JAVA_HOME/bin:$PATH
+
+jdk() {
+    version=$1
+    export JAVA_HOME=$(/usr/libexec/java_home -v"$version");
+    java -version
+}
 
 # vim
 vj() {
-  vim "+TlistAddFilesRecursive . [^_]*java"
+    vim "+TlistAddFilesRecursive . [^_]*java"
 }
